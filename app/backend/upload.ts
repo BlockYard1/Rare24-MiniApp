a6a89aa0160fd00a40704c1c07f13d2fdc868f69
calebomondi <kalebmokua@gmail.com>
2025-12-04 22:56:11 +0300

    updated to use actual moment count from contract

--- app/backend/upload.ts
+++ app/backend/upload.ts
@@ -2,19 +2,17 @@
 
 import { pinata } from "@/utils/config"
 
-// moment id from users number of deployed tokenIds
-const momentId = 1;
-
 export async function uploadImage(formData: FormData) {
   const file = formData.get('image') as File
   const caption = formData.get('caption') as string
   const creator = formData.get('creator') as string
+  const momentCount = formData.get('momentCount') as string
   
   try {
     // Pinata Upload
     const { cid } = await pinata.upload.public
       .file(file)
-      .name(`${creator}-${momentId}`)
+      .name(`${creator}-${momentCount}`)
       .keyvalues({
         type: 'nft-image',
         uploadAt: new Date().toISOString()
@@ -23,7 +21,7 @@ export async function uploadImage(formData: FormData) {
 
     // construct NFT metadata
     const nftMetadata = {
-      name: `${creator}-${momentId}`,
+      name: `${creator}-${momentCount}`,
       description: caption,
       image: imageUrl,
       attributes: [
@@ -35,7 +33,7 @@ export async function uploadImage(formData: FormData) {
     // upload metadata JSON to pinata
     const metadataUpload = await pinata.upload.public
       .json(nftMetadata)
-      .name(`${creator}-${momentId}-metadata`)
+      .name(`${creator}-${momentCount}-metadata`)
       .keyvalues({
         type: 'nft-metadata',
         uploadAt: new Date().toISOString()
