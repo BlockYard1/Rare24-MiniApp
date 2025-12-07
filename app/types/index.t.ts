563cd9b72b79374d25e9de1ea4e2a4fefa0234c3
calebomondi <kalebmokua@gmail.com>
2025-12-07 15:10:14 +0300

    initial implementation

---
+++ app/types/index.t.ts
@@ -0,0 +1,9 @@
+export interface UserData {
+    fid: number;
+    username: string;
+    displayName: string;
+    pfpUrl: string;
+    followerCount: number;
+    followingCount: number;
+    bio: string;
+}
