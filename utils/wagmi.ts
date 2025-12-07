48e0d5059014340f481f91cdf24d8430baa9d429
calebomondi <kalebmokua@gmail.com>
2025-12-07 22:59:05 +0300

    update variable naming

--- utils/wagmi.ts
+++ utils/wagmi.ts
@@ -3,10 +3,11 @@ import { baseSepolia } from 'wagmi/chains'
 import { farcasterMiniApp as miniAppConnector } from '@farcaster/miniapp-wagmi-connector'
 import { injected } from 'wagmi/connectors'
 
+
 export const config = createConfig({
   chains: [baseSepolia],
   transports: {
-    [baseSepolia.id]: http(process.env.BASE_SEPOLIA_RPC),
+    [baseSepolia.id]: http(process.env.NEXT_PUBLIC_BASE_SEPOLIA_RPC),
   },
   connectors: [
     injected(),
