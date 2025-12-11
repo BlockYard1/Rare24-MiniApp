31d05290cbcd124b47000f498e97e47816fd2982
calebomondi <kalebmokua@gmail.com>
2025-12-11 18:42:11 +0300

    added new interfaces for NFT page

--- app/types/index.t.ts
+++ app/types/index.t.ts
@@ -38,3 +38,42 @@ interface OwnedNFT {
 export interface AlchemyNFTResponse {
     ownedNfts: OwnedNFT[];
 }
+
+export interface MomentDetails {
+    creator: string;
+    imageUrl: string;
+    desc: string;
+    created: string;
+    expires: number;
+    totalMints: number;
+    maxSupply: number;
+    price: string;
+}
+
+export interface OfferListing {
+    id: number;
+    account: string;
+    amount: number;
+    price: string;
+    expires: string;
+}
+
+export interface MomentsSaleData {
+    lastSale: string;
+    collectionFloor: number;
+    highestOffer: number;
+    buyNow: OfferListing;
+    orders: OfferListing[];
+    listings: OfferListing[];
+}
+
+export interface ActivityVolumeData {
+    activity: {
+        type: string;
+        seller: string;
+        buyer: string;
+        price: string;
+        timeStamp: number;
+    }[];
+    volume: number;
+}
