2b368d21217db19a051e2a977d943013fe4f7577
calebomondi <kalebmokua@gmail.com>
2025-12-14 13:25:30 +0300

    implemented useNotificationStore

--- app/store/useFarcasterStore.ts
+++ app/store/useFarcasterStore.ts
@@ -1,12 +1,20 @@
 // store/useFarcasterStore.ts
 import { create } from 'zustand'
 import { persist } from 'zustand/middleware'
-import { UserData } from '../types/index.t'
+import { UserData, Notification } from '../types/index.t'
 
 interface FarcasterStore {
   user: UserData | null
   loading: boolean
   setUser: (user: UserData) => void
+  setLoading: (loading: boolean) => void
+  clearUser: () => void
+}
+
+interface NotificationsStore {
+  notify: Notification[] | null
+  loading: boolean
+  setNotify: (notify: Notification[]) => void
   setLoading: (loading: boolean) => void
   clearUser: () => void
 }
@@ -21,7 +29,22 @@ export const useFarcasterStore = create<FarcasterStore>()(
       clearUser: () => set({ user: null })
     }),
     {
-      name: 'farcaster-storage', // localStorage key
+      name: 'farcaster-storage', // key
+    }
+  )
+)
+
+export const useNotificationStore = create<NotificationsStore>()(
+  persist(
+    (set) => ({
+      notify: null,
+      loading: true,
+      setNotify: (notify) => set({ notify, loading: false }),
+      setLoading: (loading) => set({ loading }),
+      clearUser: () => set({ notify: null })
+    }),
+    {
+      name: 'notify-storage', // key
     }
   )
 )
