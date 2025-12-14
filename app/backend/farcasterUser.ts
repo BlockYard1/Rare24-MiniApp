5cc8286e93f27fd4ac0b3847d82b4d9355a6f3f4
calebomondi <kalebmokua@gmail.com>
2025-12-14 13:18:04 +0300

    added getAllFollowings() and added types to getFarcasterUser() return

--- app/backend/farcasterUser.ts
+++ app/backend/farcasterUser.ts
@@ -16,12 +16,37 @@ export async function getFarcasterUser(fid: number) {
   const user = data.users[0];
   
   return {
-    fid: user.fid,
-    username: user.username,
-    displayName: user.display_name,
-    pfpUrl: user.pfp_url,
-    followerCount: user.follower_count,
-    followingCount: user.following_count,
-    bio: user.profile.bio.text
+    fid: user.fid as number,
+    username: user.username as string,
+    displayName: user.display_name as string,
+    pfpUrl: user.pfp_url as string,
+    followerCount: user.follower_count as number,
+    followingCount: user.following_count as number,
+    bio: user.profile.bio.text as string
   };
+}
+
+export async function getAllFollowings(fid: number) {
+  const allFollowing: any[] = [];
+  let cursor: string | null = null;
+
+  do {
+    const url = cursor 
+      ? `https://api.neynar.com/v2/farcaster/following/?limit=100&fid=${fid}&cursor=${cursor}`
+      : `https://api.neynar.com/v2/farcaster/following/?limit=100&fid=${fid}`;
+
+    const response = await fetch(url, {
+      headers: {
+        'Content-Type': 'application/json',
+        'x-api-key': process.env.NEYNAR_API_KEY!
+      }
+    });
+
+    const data: any = await response.json();
+    allFollowing.push(...data.users);
+    cursor = data.next.cursor;
+
+  } while (cursor);
+
+  return allFollowing.map(account => account.user.username as string)
 }
