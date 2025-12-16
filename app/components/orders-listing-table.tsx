bea44730ff59b2b80914ccf15d21b636ff0f3719
calebomondi <kalebmokua@gmail.com>
2025-12-16 13:04:15 +0300

    added ETH icon to price column

--- app/components/orders-listing-table.tsx
+++ app/components/orders-listing-table.tsx
@@ -20,7 +20,26 @@ export default function OrdersListingTable({ data, tab } : { data: OfferListing[
               {
                 data.map((order, idx) => (
                   <tr key={idx} className="border-b border-gray-500/30">
-                    <td className="py-3 px-0 font-semibold">{order.price} ETH</td>
+                    {/* <td className="py-3 px-0 font-semibold">{order.price} ETH</td> */}
+                    <td className="py-3 px-0 font-semibold">
+                      <div className="flex items-center gap-1">
+                        <img 
+                          src="/eth_light.png" 
+                          alt={order.account}
+                          className="w-3 object-cover hidden dark:block"
+                        />
+                        <img 
+                          src="/eth_dark.png" 
+                          alt={order.account}
+                          className="w-5 object-cover dark:hidden"
+                        />
+                        <p className="text-lg font-semibold">
+                          <span className="font-semibold">
+                            {order.price}
+                          </span>
+                        </p>
+                      </div>
+                    </td>
                     <td className="py-3 px-4">{order.amount}</td>
                     <td className="py-3 px-4 text-muted-foreground">{order.account}</td>
                     <td className="py-3 px-4 text-muted-foreground">{order.expires}</td>
