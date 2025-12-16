2bc9b78c2d413979456c5ea4af52723f76452675
calebomondi <kalebmokua@gmail.com>
2025-12-16 13:10:52 +0300

    updated styling

--- app/notifications/page.tsx
+++ app/notifications/page.tsx
@@ -16,22 +16,29 @@ export default function Notifications() {
             {
               notify?.map(notification => {
                 return (
-                  <div className="border-b border-gray-500/30" key={notification.imageUrl}>
+                  <div className="border-b-1 border-gray-500/30" key={notification.imageUrl}>
                     {/* Image */}
                     <div 
-                      className="border-b max-h-[25%] p-1 flex items-center justify-center"
+                      className="p-2 flex items-center justify-evenly"
                       onClick={() => {
                         route.push(`nft/${notification.tokenId}`)
                       }}
                     >
-                      <img 
-                        src={notification?.imageUrl} 
-                        alt={notification?.buyer}
-                      />
+                      <div className="relative aspect-square w-20 grid place-items-center shrink-0 overflow-hidden rounded-md">
+                        <img 
+                          src={notification?.imageUrl || "/placeholder.svg"} 
+                          alt={notification.buyer} 
+                          className="object-cover"
+                        />
+                      </div>
                       <div>
-                        <p>
-                          <span>{notification.buyer}</span>
-                          <span>{notification.price}</span>
+                        <p className="p-2 text-lg text-gray-700 dark:text-gray-300">
+                          Offer for token
+                          <span className="font-semibold"> #{notification.tokenId} </span>
+                           from
+                          <span className="font-semibold"> {notification.buyer} </span>
+                          @
+                          <span className="font-semibold"> {notification.price} ETH</span>
                         </p>
                       </div>
                     </div>
@@ -42,8 +49,12 @@ export default function Notifications() {
           </>
         ) : (
           <>
-            <BellMinus size={30} className="text-teal-500/80 mb-3"/>
-            <span className="text-lg text-teal-600/80">No Notifications</span>
+            <div className="flex items-center justify-center" style={{ minHeight: 'calc(100vh - 8rem)' }}>
+              <div className="flex flex-col items-center justify-center">
+                <BellMinus size={60} className="text-teal-500/80 mb-3"/>
+                <span className="text-lg text-teal-600/80">No Notifications</span>
+              </div>
+            </div>
           </>
         )
       }
