1dae2465e498d6c5e04b6c26bb50e2c90d5569b8
calebomondi <kalebmokua@gmail.com>
2025-12-18 18:06:05 +0300

    commented out unused functions

--- app/backend/farcasterUser.ts
+++ app/backend/farcasterUser.ts
@@ -1,5 +1,11 @@
 "use server"
 
+// import { NeynarAPIClient, Configuration } from "@neynar/nodejs-sdk";
+
+// const client = new NeynarAPIClient(
+//   new Configuration({ apiKey: process.env.NEYNAR_API_KEY ?? '' })
+// );
+
 export async function getFarcasterUser(fid: number) {
   const response = await fetch(
     `https://api.neynar.com/v2/farcaster/user/bulk?fids=${fid}`,
@@ -36,54 +42,42 @@ export async function getFarcasterUser(fid: number) {
 //       : `https://api.neynar.com/v2/farcaster/following/?limit=100&fid=${fid}`;
 
 //     const response = await fetch(url, {
+//       method: 'GET',
 //       headers: {
 //         'Content-Type': 'application/json',
-//         'x-api-key': process.env.NEYNAR_API_KEY!
 //       }
 //     });
 
+//     if (!response.ok) {
+//       throw new Error(`API request failed: ${response.status}`);
+//     }
+
 //     const data: any = await response.json();
-//     allFollowing.push(...data.users);
+    
+//     // Check if users array exists and is an array
+//     if (data.users && Array.isArray(data.users)) {
+//       allFollowing.push(...data.users);
+//     } else {
+//       console.error('Unexpected API response structure:', data);
+//       break;
+//     }
+    
 //     cursor = data.next?.cursor || null;
 
 //   } while (cursor);
 
-//   return allFollowing.map(account => account.user.username as string)
+//   return allFollowing.map(account => account.username as string);
 // }
 
-export async function getAllFollowings(fid: number) {
-  const allFollowing: any[] = [];
-  let cursor: string | null = null;
-
-  do {
-    const url = cursor 
-      ? `https://api.neynar.com/v2/farcaster/following/?limit=100&fid=${fid}&cursor=${cursor}`
-      : `https://api.neynar.com/v2/farcaster/following/?limit=100&fid=${fid}`;
-
-    const response = await fetch(url, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      }
-    });
-
-    if (!response.ok) {
-      throw new Error(`API request failed: ${response.status}`);
-    }
-
-    const data: any = await response.json();
+// export async function searchUsers(query: string) {
+//   try {
+//     const res = await client.searchUser({
+//       q: query,
+//       limit: 100,
+//     });
     
-    // Check if users array exists and is an array
-    if (data.users && Array.isArray(data.users)) {
-      allFollowing.push(...data.users);
-    } else {
-      console.error('Unexpected API response structure:', data);
-      break;
-    }
-    
-    cursor = data.next?.cursor || null;
-
-  } while (cursor);
-
-  return allFollowing.map(account => account.username as string);
-}
+//     return res
+//   } catch (err) {
+//     console.error(`Error searching users: ${err}`);
+//   }
+// }
