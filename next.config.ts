dab03bf7ab34cdf388438f0515e8a5777fab9b25
calebomondi <kalebmokua@gmail.com>
2025-12-23 00:07:49 +0300

    reverted back to local manifest

--- next.config.ts
+++ next.config.ts
@@ -8,15 +8,15 @@ const nextConfig: NextConfig = {
     },
   },
   /* Redirects */
-  async redirects() {
-    return [
-      {
-        source: '/.well-known/farcaster.json',
-        destination: 'https://api.farcaster.xyz/miniapps/hosted-manifest/019b47d5-5a0f-21b9-afc8-478832a6bc85',
-        permanent: false,
-      },
-    ]
-  },
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: '/.well-known/farcaster.json',
+  //       destination: 'https://api.farcaster.xyz/miniapps/hosted-manifest/019b47d5-5a0f-21b9-afc8-478832a6bc85',
+  //       permanent: false,
+  //     },
+  //   ]
+  // },
 };
 
 export default nextConfig;
