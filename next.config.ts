8bf34ee1489c0b24b4b9e1b12b8221a16e3c844a
calebomondi <kalebmokua@gmail.com>
2025-12-23 00:00:18 +0300

    update to use remote manifest

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,16 @@ const nextConfig: NextConfig = {
       bodySizeLimit: '2mb',
     },
   },
+  /* Redirects */
+  async redirects() {
+    return [
+      {
+        source: '/.well-known/farcaster.json',
+        destination: 'https://api.farcaster.xyz/miniapps/hosted-manifest/019b47d5-5a0f-21b9-afc8-478832a6bc85',
+        permanent: false,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
