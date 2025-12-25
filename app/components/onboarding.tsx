07db1e870c1b15fe427b8d1c99f772561a31e94a
calebomondi <kalebmokua@gmail.com>
2025-12-25 03:13:12 +0300

    refactor: enhance onboarding flow with improved titles and descriptions

--- app/components/onboarding.tsx
+++ app/components/onboarding.tsx
@@ -10,15 +10,31 @@ export default function OnboardingFlow({ onComplete }: { onComplete: () => void 
   const steps = [
     {
       imageUrl: '/rare24.png',
-      title: "Welcome",
+      title: "Welcome to Rare24",
       description: <div className='text-gray-700 dark:text-gray-300'>
-        <p className='text-center text-xl font-semibold'>Own Your Favorite Creators' Rare Moments</p>
-        <ol className='text-lg text-gray-600 dark:text-gray-200 mt-4 space-y-1 list-decimal pl-5'>
-            <li className='text-start'>Share a moment with a caption, set the max supply and price</li>
-            <li className='text-start'>On the feed and marketplace, like a moment to buy it</li>
-            <li className='text-start'>Make an offer for a moment or resell a moment you own</li>
-        </ol>
-    </div>
+        <p className='text-center text-lg font-semibold'>Own Your Favorite Creators' Rare Moments</p>
+      </div>
+    },
+    {
+      imageUrl: '/share.png',
+      title: "Share Moments",
+      description: <div className='text-gray-700 dark:text-gray-300'>
+        <p className='text-center text-lg font-semibold'>Select a moment, set the max supply and price then share it</p>
+      </div>
+    },
+    {
+      imageUrl: '/mint.png',
+      title: "Buy Moments",
+      description: <div className='text-gray-700 dark:text-gray-300'>
+        <p className='text-center text-lg font-semibold'>On the feed and marketplace, ❤️ a moment to buy it</p>
+      </div>
+    },
+    {
+      imageUrl: '/offer.png',
+      title: "Make Offers & Resell",
+      description: <div className='text-gray-700 dark:text-gray-300'>
+        <p className='text-center text-lg font-semibold'>Make an offer for a moment or resell a moment you own</p>
+      </div>
     }
   ];
 
@@ -45,17 +61,17 @@ export default function OnboardingFlow({ onComplete }: { onComplete: () => void 
 
   return (
     <div className="fixed inset-0 bg-black/70 bg-opacity-50 flex items-center justify-center z-50 p-4">
-      <div className={`bg-background rounded-2xl max-w-md w-full max-h-[90vh] p-8 relative transform transition-all duration-300 flex flex-col ${isVisible ? 'scale-100 opacity-100' : 'scale-95 opacity-0'}`}>
+      <div className={`bg-background rounded-2xl max-w-md w-full max-h-[90vh] px-3 py-3 relative transform transition-all duration-300 flex flex-col ${isVisible ? 'scale-100 opacity-100' : 'scale-95 opacity-0'}`}>
         {/* Skip button */}
-        <button
+        {/* <button
           onClick={handleSkip}
           className="absolute top-4 right-4 text-gray-400 hover:text-gray-600"
         >
           <X className="w-6 h-6" />
-        </button>
+        </button> */}
 
         {/* Progress indicators */}
-        <div className="flex justify-center gap-2 mb-8">
+        <div className="flex justify-center gap-2 my-5">
           {steps.map((_, index) => (
             <div
               key={index}
@@ -77,7 +93,7 @@ export default function OnboardingFlow({ onComplete }: { onComplete: () => void 
                     <img 
                         src={steps[currentStep].imageUrl} 
                         alt={steps[currentStep].imageUrl}
-                        className="max-h-[10vh] w-auto h-auto object-contain relative z-10"
+                        className="max-h-[10vh] w-full relative z-10"
                     />
                 </div>
                 
@@ -90,15 +106,15 @@ export default function OnboardingFlow({ onComplete }: { onComplete: () => void 
                 </p>
             </div>
             {/* Actions */}
-            <div className='flex flex-center justify-center'>
+            <div className='flex flex-center justify-between'>
                 {/* Skip button */}
-                {/* <button
+                <button
                     onClick={handleSkip}
                     className="px-4 py-3 rounded-full font-medium flex items-center justify-center border border-teal-500 dark:border-teal-800 gap-2"
                 >
                     <SkipForward className="w-5 h-5" />
                     <span>Skip</span>
-                </button> */}
+                </button>
                 {/* Next/Complete button */}
                 <button
                     onClick={handleNext}
